e38ffcd82e7ae993850582251e1f410eed919cec
Elvis Ibarra <ielvisd@gmail.com>
2019-02-21 15:35:16 -0800

    rps.py doesn't output in correct order but solves teh problem

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -5,8 +5,8 @@ import sys
 
 def rock_paper_scissors(n):
 		rps = ["rock", "paper", "scissors"]
-		allPlays = [['game']]
-		plays = [allPlays * 3**((n))]
+		allPlays = [[]]
+		plays = allPlays * 3**((n))
 		print(f"I'm the plays: {plays}")
 		if n <= 0:
 				return plays
@@ -14,23 +14,28 @@ def rock_paper_scissors(n):
 				print(f" i'm the round: {roundNumber}")
 				print(f" i'm allPlays: {allPlays}")
 				if n == 1:
-						allPlays = [['rock'], ['paper'], ['scissors']]
+						plays = [['rock'], ['paper'], ['scissors']]
 						pass
 				else:
-						for n in range(n, n+1):
-							print(f" round {n}")
-							for i in rps:
-								allPlays.append(i)
-							print(f"test allPlays {allPlays}")
-						# for i in allPlays:
-						# 		for j in range(0,3):
-						# 			# allplays
-						# 			i.append(rps[j])
-						# 			print(f"I'm testttt allPlays {allPlays}")
-						# 			pass
+						print(f"plays is this long: {len(plays)}")
+						for x in range(0, len(plays), 3):
+							print(f" x is {x}")
+							print(f" all plays at {x} is: {plays[x]}")
+							plays[x] = [rps[0], rps[x // 3]]
+							print(f"test plays {plays}")
+						for y in range(1, len(plays), 3):
+							print(f" round {y}")
+							print(f" all plays at {y} is: {plays[y]}")
+							plays[y] = [rps[1], rps[y // 3]]
+							print(f"test plays {plays}")
+						for z in range(2, len(plays), 3):
+							print(f" round {z}")
+							print(f" all plays at {z} is: {plays[z]}")
+							plays[z] = [rps[2], rps[z // 3]]
+							print(f"test plays {plays}")
 
-		print(f" i'm Final allPlays: {allPlays}")
-		return allPlays
+		print(f" i'm Final allPlays: {plays}")
+		return plays
 
 
 if __name__ == "__main__":
